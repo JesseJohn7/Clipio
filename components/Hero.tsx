c3cf479a7b3a8a472df0a5ba117889f665c6dd12
JesseJohn7 <Jessejohn260@gmail.com>
2026-05-15 10:04:54 +0100

    paragraph text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -103,7 +103,7 @@ export default function Hero() {
       </h1>
 
       <p className="text-zinc-400 text-base md:text-lg text-center max-w-xl mb-12">
-        Paste a link from TikTok, X, Facebook, Instagram and more — download instantly in full quality.
+       Fast and effortless video downloads from TikTok, Instagram, X, Facebook, and more in full quality.
       </p>
 
       {/* Input Box */}
