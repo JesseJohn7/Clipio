e48237ac3151ccc45812e1eabb79ea77a9350aca
JesseJohn7 <Jessejohn260@gmail.com>
2026-05-18 09:57:34 +0100

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://clipio-tau.vercel.app"),
   title: "Clipio • Download videos instantly",
   description:
     "Download videos from TikTok, Instagram, X, Facebook, and more instantly in full quality.",
